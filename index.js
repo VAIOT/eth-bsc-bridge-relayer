833057e5e61620070a1a3b26ac4d06c20880df08
Dajanaaa <diana.piotrowska17@gazeta.pl>
2021-05-27 08:30:33 +0200

    add kube-probe

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const listener = require('./services/listener')
+const probe = require('kube-probe');
 
 const app = express()
 
@@ -13,3 +14,5 @@ module.exports = app
 if ((process.env.MODE != "eth_to_bsc") && (process.env.MODE != "bsc_to_eth")) {
   server.close()
 }
+
+probe(app); 
